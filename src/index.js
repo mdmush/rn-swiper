f341e8ad799f2120758fb1c99189b2ca893ed316
Mushfique <mushfique6@gmail.com>
2020-08-31 20:26:13 +0530

    ios crashing fix

--- src/index.js
+++ src/index.js
@@ -292,12 +292,12 @@ export default class extends Component {
     // related to https://github.com/mdmush/rn-swiper/issues/570
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
-    if (Platform.OS === 'ios') {
-      if (this.initialRender && this.state.total > 1) {
-        this.scrollView.scrollTo({...offset, animated: false})
-        this.initialRender = false;
-      }
-    }
+    // if (Platform.OS === 'ios') {
+    //   if (this.initialRender && this.state.total > 1) {
+    //     this.scrollView.scrollTo({...offset, animated: false})
+    //     this.initialRender = false;
+    //   }
+    // }
 
     this.setState(state)
   }
@@ -463,11 +463,11 @@ export default class extends Component {
     if (state.dir === 'x') x = diff * state.width
     if (state.dir === 'y') y = diff * state.height
 
-    if (Platform.OS !== 'ios') {
+    // if (Platform.OS !== 'ios') {
       this.scrollView && this.scrollView[animated ? 'setPage' : 'setPageWithoutAnimation'](diff)
-    } else {
-      this.scrollView && this.scrollView.scrollTo({ x, y, animated })
-    }
+    // } else {
+    //   this.scrollView && this.scrollView.scrollTo({ x, y, animated })
+    // }
 
     // update scroll state
     this.internals.isScrolling = true
@@ -476,7 +476,7 @@ export default class extends Component {
     })
 
     // trigger onScrollEnd manually in android
-    if (!animated || Platform.OS !== 'ios') {
+    if (!animated) {
       setImmediate(() => {
         this.onScrollEnd({
           nativeEvent: {
